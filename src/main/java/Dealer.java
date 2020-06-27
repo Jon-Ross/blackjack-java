3ee977cfd897c2124164c0c59a4a5a2587ab5ff1
JR <jon-ross.rodrigues@depop.com>
2020-06-27 03:15:35 +0100

    removed cached files

--- src/main/java/Dealer.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class Dealer {
-
-    public int dealCard() {
-        final Random rand = new Random();
-        return rand.nextInt(11);
-    }
-}
--- src/main/java/Winner.java
+++
@@ -1,4 +0,0 @@
-enum Winner {
-    HOUSE,
-    PLAYER
-}
