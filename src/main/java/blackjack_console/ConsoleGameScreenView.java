9f83ae40b72742e86e397abbac7e709e2d2c6122
ethanlaudepop <ethan.lau@depop.com>
2020-08-14 16:58:45 +0100

    added print to console functionality. Takes in user input to start game

--- src/main/java/blackjack_console/ConsoleGameScreenView.java
+++ src/main/java/blackjack_console/ConsoleGameScreenView.java
@@ -4,31 +4,51 @@ import blackjack_core.GameScreenContract;
 import blackjack_core.Hand;
 import blackjack_core.Winner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleGameScreenView implements GameScreenContract.View {
+
+    private final GameScreenContract.Presenter presenter;
+
+    public ConsoleGameScreenView(GameScreenContract.Presenter presenter) {
+        this.presenter = presenter;
+    }
 
     @Override
     public void showStartingInstructions(String instructions) {
         println(instructions);
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            final String answer = reader.readLine();
+            if (answer.toLowerCase().equals("n")) {
+                presenter.onStartBlackJackGame();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void showPlayerHand(Hand hand) {
-
+        println("Your hand is: " + hand);
     }
 
     @Override
     public void showGameInstructions(String instructions) {
         println(instructions);
+
     }
 
     @Override
     public void showHouseHand(Hand hand) {
-
+        println("House hand is: " + hand);
     }
 
     @Override
     public void showWinner(Winner winner) {
-
+        println("Winner is: " + winner);
     }
 
     @Override
